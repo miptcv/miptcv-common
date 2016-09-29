284ea0aeee0fba8850bab3806a6872c9b21c6958
Alexander Zhukovsky <zhukovsky@phystech.edu>
2016-09-29 22:29:04 +0300

    FIX: str->number conversion

--- hw#1/autocontrast.py
+++ hw#1/autocontrast.py
@@ -10,6 +10,9 @@ def autocontrast(src_path, dst_path, white_perc, black_perc):
 if __name__ == '__main__':
     assert len(argv) == 5
     assert os.path.exists(argv[1])
+    argv[3] = float(argv[3])
+    argv[4] = float(argv[4])
+
     assert 0 <= argv[3] < 1
     assert 0 <= argv[4] < 1
 
--- hw#1/gamma_correction.py
+++ hw#1/gamma_correction.py
@@ -10,5 +10,7 @@ def gamma_correction(src_path, dst_path, a, b):
 if __name__ == '__main__':
     assert len(argv) == 5
     assert os.path.exists(argv[1])
+    argv[3] = float(argv[3])
+    argv[4] = float(argv[4])
 
     gamma_correction(*argv[1:])
